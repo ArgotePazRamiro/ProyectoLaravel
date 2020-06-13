7e657a41cb106e080f8a5490106791418defc9a1
aqm1290 <aqm1290@gmail.com>
2020-06-13 18:01:55 -0400

    2cambio

--- app/Http/Controllers/histoClinicaController.php
+++ app/Http/Controllers/histoClinicaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ingrePacienteController extends Controller
+class histoClinicaController extends Controller
 {
-    //
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,3 @@ Route::view('/historiaClinica','historiaClinica')->name('historiaClinica');
 
 
 Route::view('/nuevoEmpleado','nuevoEmpleado')->name('nuevoEmpleado');
-
-Route::view('/login','login')->name('login');
