04079fb53cba4d1ff675029cc73fe08b8d9accd7
aqm1290 <aqm1290@gmail.com>
2020-06-17 21:16:43 -0400

    CAMBIO

--- resources/views/listaPacientes/edit.blade.php
+++ resources/views/listaPacientes/edit.blade.php
@@ -4,156 +4,18 @@
 
 
 @section('content')
-<br>
-<br>
-<br>
 
-<head>
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            -ms-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-
-        .container {
-            max-width: 960px;
-        }
-
-        .lh-condensed {
-            line-height: 1.25;
-        }
-    </style>
-</head>
-<form method="POST" action="{{ route('listaPacientes.update', $listaPacientes) }}">
-    @csrf @method('PATCH')
-<body class="bg-light">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 order-md-2 mb-4">
-            </div>
-            <div class="col-md-8 order-md-1">
-                <h4 class="mb-3">EDITAR PACIENTE</h4>
-                <form class="needs-validation" novalidate>
-                    <div class="mb-3">
-                        <label for="nroDocumento">Carnet de Identidad</label>
-                    <input type="text" class="form-control" id="nroDocumento" name="nroDocumento" value="{{old('nroDocumento', $listaPacientes ->nroDocumento)}}" required>
-                        <div class="invalid-feedback">
-                            Por favor introduzca su carnet de identidad.
-                        </div>
-                    </div>
-
-
-
-                    <div class="mb-3">
-                        <label for="nombres">Nombre</label>
-                        <input type="text" class="form-control" id="nombres" name="nombres" value="{{old('nombres', $listaPacientes ->nombres)}}" required>
-                        <div class="invalid-feedback">
-                            Por favor introduzca su nombre.
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label for="apPaterno">Apellido Paterno</label>
-                            <input type="text" class="form-control" id="apPaterno" name="apPaterno" pattern="[A-Za-z]+" minlength="2" maxlength="20" value="{{$listaPacientes ->apPaterno}}"required >
-                            <div class="invalid-feedback">
-                                Por favor introduzca su apellido paterno.
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="apMaterno">Apellido Materno</label>
-                            <input type="text" class="form-control" id="apMaterno" name="apMaterno" pattern="[A-Za-z]+" minlength="2" maxlength="20"value="{{$listaPacientes ->apMaterno}}" required >
-                            <div class="invalid-feedback">
-                                Por favor introduzca su apellido materno.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-5 mb-3">
-                            <label for="sexo">Sexo</label>
-                            <select class="custom-select d-block w-100" id="sexo" name="sexo" value="{{$listaPacientes ->sexo }}" required>
-                                <option> </option>
-                                <option>Masculino</option>
-                                <option>Femenino</option>
-                                <option>Otro</option>
-                            </select>
-                            <div class="invalid-feedback">
-                                Seleccione un sexo valido.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="telefonno">Telefono</label>
-                        <input type="text" class="form-control" id="telefonno" name="telefonno" minlength="7" pattern="[0-9]+" value="{{$listaPacientes ->telefonno}}" required >
-                        <div class="invalid-feedback">
-                            Por favor introduzca su telefono.
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="edad">Edad</label>
-                        <input type="text" class="form-control" id="edad" name="edad" value="{{$listaPacientes ->edad}}" required>
-                        <div class="invalid-feedback">
-                            Por favor introduzca su edad.
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="direccion">Direccion</label>
-                        <input type="text" class="form-control" id="direccion" name="direccion" pattern="[A-Za-z0-9]+" minlength="2" maxlength="40" value="{{$listaPacientes ->direccion}}"required>
-                        <div class="invalid-feedback">
-                            Por favor introduzca su direccion.
-                        </div>
-                    </div>
-                    <hr class="mb-4">
-                    <button class="btn btn-success btn-lg" type="submit">Actualizar</button>
-                </form>
-
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-sm-10 col-lg-6 mx-auto">
+            <form method="POST" action="{{ route('listaPacientes.update', $listaPacientes) }}">
+                 @method('PATCH')
+                <h1 class="display-4">Editar Paciente</h1>
+                <hr>
+                @include('listaPacientes._form',['btnText'=>'Actualizar'])
+            </form>
         </div>
-
-        <footer class="my-5 pt-5 text-muted text-center text-small">
-            <p class="mb-1">&copy; 2017-2020 Servidores A y R</p>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Privacy</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Support</a></li>
-            </ul>
-        </footer>
     </div>
-</form>
-    <script>
-        (function () {
-            'use strict'
-
-            window.addEventListener('load', function () {
-        
-                var forms = document.getElementsByClassName('needs-validation')
-
-                Array.prototype.filter.call(forms, function (form) {
-                    form.addEventListener('submit', function (event) {
-                        if (form.checkValidity() === false) {
-                            event.preventDefault()
-                            event.stopPropagation()
-                        }
-                        form.classList.add('was-validated')
-                    }, false)
-                })
-            }, false)
-        }())
-    </script>
-</body>
-
-
+</div>
 
 @endsection
